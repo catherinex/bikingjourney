bc7c232ee708606ec0e35327af56bc819867b9b6
catherinex <demonxuxiang@gmail.com>
2014-12-16 12:32:34 +0100

    add data validation

--- src/com/xiangxu/bikingjourney/NewJourneyActivity.java
+++ src/com/xiangxu/bikingjourney/NewJourneyActivity.java
@@ -1,17 +1,12 @@
 package com.xiangxu.bikingjourney;
 
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.ParseException;
 import java.util.Calendar;
-import java.util.Date;
 
 import com.google.gson.Gson;
+import com.xiangxu.bikingjourney.database.JourneyDataSource;
 
 import android.support.v7.app.ActionBarActivity;
 import android.app.Fragment;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -21,18 +16,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class NewJourneyActivity extends ActionBarActivity {
+	
+	private Journey journey;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_journey);
+		
+		
+		
+		String json = getIntent().getStringExtra("journey");
+		if (json != null)
+			journey = new Gson().fromJson(json, Journey.class);
+		
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+					.add(R.id.container, new PlaceholderFragment(journey)).commit();
 		}
 		
 		
@@ -75,30 +78,50 @@ public class NewJourneyActivity extends ActionBarActivity {
         private Button btnStartDate;
         private EditText etName;
         private Button btnEndDate;
+        private JourneyDataSource datasource;
+        private Journey journey;
+        private AlertDialogManager alert = new AlertDialogManager();
         
 
-		public PlaceholderFragment() {
+		public PlaceholderFragment(Journey journey) {
+			this.journey = journey;
 		}
+
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
+			
+			datasource = new JourneyDataSource(getActivity());
+		    datasource.open();
+		    
 			View rootView = inflater.inflate(R.layout.fragment_new_journey,
 					container, false);
 			
 			etName = (EditText)rootView.findViewById(R.id.et_name);
-			
-			final Calendar c = Calendar.getInstance();
-	        year = c.get(Calendar.YEAR);
-	        month = c.get(Calendar.MONTH); // Month is 0 based
-	        day = c.get(Calendar.DAY_OF_MONTH);
-	        
-	        startDate = c;
-	        endDate = c;
-	   
-	        	        
 			btnStartDate = (Button)rootView.findViewById(R.id.btn_startdate);
-			btnStartDate.setText(day + "/" + (month + 1) + "/" + year);
+			btnEndDate = (Button)rootView.findViewById(R.id.btn_enddate);
+			
+			// set widgets
+			if (journey == null) {
+				final Calendar c = Calendar.getInstance();
+				year = c.get(Calendar.YEAR);
+				month = c.get(Calendar.MONTH); // Month is 0 based
+				day = c.get(Calendar.DAY_OF_MONTH);
+	        
+				startDate = c;
+				endDate = c;
+	   
+	        	btnStartDate.setText(year + "-" + (month+1) + "-" + day);        
+	        	btnEndDate.setText(year + "-" + (month+1) + "-" + day);  
+			} else {
+				startDate = journey.getStartDate();
+				endDate = journey.getEndDate();
+				etName.setText(journey.getName());
+				btnStartDate.setText(Utility.calendarToDate(startDate));
+				btnEndDate.setText(Utility.calendarToDate(endDate));
+				
+			}
 			
 			btnStartDate.setOnClickListener(new View.OnClickListener() {
 				
@@ -114,7 +137,7 @@ public class NewJourneyActivity extends ActionBarActivity {
 					    	year = startDate.get(Calendar.YEAR);
 					    	month = startDate.get(Calendar.MONTH);
 					    	day = startDate.get(Calendar.DAY_OF_MONTH);
-					    	btnStartDate.setText(day + "/" + (month + 1) + "/" + year);
+					    	btnStartDate.setText(year + "-" + (month+1) + "-" + day);  
 					    }
 					});
 					ddf.show(getFragmentManager(), "date picker dialog fragment");
@@ -123,8 +146,8 @@ public class NewJourneyActivity extends ActionBarActivity {
 			
 			
 			
-			btnEndDate = (Button)rootView.findViewById(R.id.btn_enddate);
-			btnEndDate.setText(day + "/" + (month + 1) + "/" + year);
+			
+			
 			btnEndDate.setOnClickListener(new View.OnClickListener() {
 				
 				@Override
@@ -139,7 +162,7 @@ public class NewJourneyActivity extends ActionBarActivity {
 					    	year = endDate.get(Calendar.YEAR);
 					    	month = endDate.get(Calendar.MONTH);
 					    	day = endDate.get(Calendar.DAY_OF_MONTH);
-					    	btnEndDate.setText(day + "/" + (month + 1) + "/" + year);
+					    	btnEndDate.setText(year + "-" + (month+1) + "-" + day);
 					    }
 					});
 					ddf.show(getFragmentManager(), "date picker dialog fragment");
@@ -147,37 +170,49 @@ public class NewJourneyActivity extends ActionBarActivity {
 			});
 			
 			Button btnSave = (Button)rootView.findViewById(R.id.btn_save_newjourney);
+			
+			// click save and next step button
 			btnSave.setOnClickListener(new View.OnClickListener() {
 				
 				@Override
 				public void onClick(View v) {
-					// TODO Auto-generated method stub
-					String id = "J" + Integer.toString(GlobalData.journeyList.size() + 1);
+					// check name
 					String name = etName.getText().toString();
+					if (name.equals("")) {
+						alert.showAlertDialog(getActivity(), "Data invalid",
+                                "Please input the name!",
+                                false);
+						return;
+					}
+					
+					// check date
+					if (endDate.before(startDate)) {
+						alert.showAlertDialog(getActivity(), "Data invalid", "End Date must be same with or after Start Date!", false);
+						return;
+					}
 					long milliSec1 = startDate.getTimeInMillis();
 			        long milliSec2 = endDate.getTimeInMillis();
 			        long timeDifInMilliSec = milliSec2 - milliSec1;
 			        int timeDifDays = (int)timeDifInMilliSec / (24 * 60 * 60 * 1000) + 1;
+				    
+				    // save to database
+			        if (journey == null)
+			        	journey = datasource.createJourney(name, startDate, endDate, timeDifDays);
+			        else {
+			        	journey.setName(name);
+			        	journey.setStartDate(startDate);
+			        	journey.setEndDate(endDate);
+			        	journey.setNDays(timeDifDays);
+			        	datasource.updateJourney(journey.getId(), name, startDate, endDate, timeDifDays);
+			        }
 			        
-					
-					Journey journey = new Journey(id, name, startDate, endDate, timeDifDays);
-					GlobalData.journeyList.add(journey);
-					
-					// Convert the object to a JSON string
-				    String json = new Gson().toJson(GlobalData.journeyList);
-				    //Toast.makeText(getActivity(), json, Toast.LENGTH_SHORT).show();
+			     // prepare json string
+					String s = new Gson().toJson(journey);
+				    Intent returnIntent = new Intent();
+					returnIntent.putExtra("journey_saved",s);
+					getActivity().setResult(RESULT_OK,returnIntent);
 				    
-				    String filename = "journey.json";
-				    FileOutputStream outputStream;
-
-				    try {
-				    	outputStream = getActivity().openFileOutput(filename, Context.MODE_PRIVATE);
-				    	outputStream.write(json.getBytes());
-				    	outputStream.close();
-				    } catch (Exception e) {
-				    	e.printStackTrace();
-				    }
-				    
+				    // go to ToursActivity
 				    Intent intent = new Intent(getActivity(), ToursActivity.class);
 				    intent.putExtra("journey", new Gson().toJson(journey));
 					startActivity(intent);
@@ -186,6 +221,18 @@ public class NewJourneyActivity extends ActionBarActivity {
 			});
 			return rootView;
 		}
+		
+		@Override
+		public void onResume() {
+		    datasource.open();
+		    super.onResume();
+		  }
+
+		  @Override
+		public void onPause() {
+		    datasource.close();
+		    super.onPause();
+		  }
 		
 	}
 		
